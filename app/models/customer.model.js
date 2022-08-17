9040cbff90ed7b33675e8dcdabf417bfee9c6061
mateoggv94 <mateoggv94@fpuna.edu.py>
2022-08-17 04:10:48 -0400

    change cors origin

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -1,9 +1,16 @@
+
 module.exports = (sequelize, Sequelize) => {
+  
   const customer = sequelize.define("customer", {
     nombre: {
       type: Sequelize.STRING
     },
   });
+  
+  //const cuota = --
+
+
+ // customer.belongsTo(cuota); // foreign in customer
 
   return customer;
 };
