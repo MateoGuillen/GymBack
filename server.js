2cff0c95f0624b6055b447dc8f00995a1ee5f7f7
mateoggv94 <mateoggv94@fpuna.edu.py>
2022-09-15 00:03:48 -0400

    test cyclic in production

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
   jwt({
     secret: process.env.PRIVATE_KEY,
     algorithms: ["HS256"],
-  }).unless({ path: ["/api/users/login", "/api/users/signup"] })
+  }).unless({ path: ["/api/users/login", "/api/users/signup", "/api/users", "/"] })
 );
 
 /*
